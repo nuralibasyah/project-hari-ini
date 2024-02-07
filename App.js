4b7a9dabd10fb64662814c58165cbd8f885a880b
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-07 09:45:51 +0700

    add some changes in src

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
